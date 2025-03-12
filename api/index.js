757dbf340fb251a4ec5d4c1ac6a42b26cfd5e8fb
Manas Adhikari <152010406+Manasadhikari05@users.noreply.github.com>
2025-03-12 20:49:58 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const app=express();
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
+const port =3000;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/come",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.post("/response",(req,res)=>{
+    res.render("response.ejs");
+});
+
+
+
+app.listen(port,()=>{
+    console.log(`listening on the port number ${port}`);
+});
