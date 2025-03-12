ac5f45f84e05066df691191a389aafae36c1b54b
Manas Adhikari <152010406+Manasadhikari05@users.noreply.github.com>
2025-03-12 22:14:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/come", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
